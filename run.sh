257fb2556aaf3f856a1dc7b92a8dd48b74c7b345
Ka <k@ka.st>
2018-05-17 01:28:00 +0200

    Moved Domoticz db file to separate folder to me mounted as a Docker volume

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+mkdir -p /opt/domoticz
+
 while true; do
    if [ -f /stop ]; then
       exit 1
    else
-      /domoticz/domoticz
+      /domoticz/domoticz -dbase /opt/domoticz/domoticz.db
    fi
 done
 		
